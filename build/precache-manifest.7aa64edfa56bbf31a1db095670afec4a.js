55dc6146df6126f79cfd5dbbba7eba1c275a1605
Mykhail1387 <melnychukmykhailo87@gmail.com>
2020-03-16 13:40:14 +0200

    my second home work phonebook

---
+++ build/precache-manifest.7aa64edfa56bbf31a1db095670afec4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd3be9779f25d972e6e176aa49bae3f",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "f49ffaa1365af7a56f02",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.68d36e8c.chunk.css"
+  },
+  {
+    "revision": "7a5f151c3c4240ae0786",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.b7d708d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.b7d708d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49ffaa1365af7a56f02",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.f561978f.chunk.js"
+  },
+  {
+    "revision": "238a1528aed3a14ce705",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d660e622.js"
+  }
+]);
